9aadbd49691185f9504b7d89674c360d6c5cc58b
cristopher-CJ <christopher.barahona@aetech.biz>
2025-05-28 15:56:29 +0200

    SAMPLE PRUEBA 27

--- annexosBASE/src/main.tsx
+++ annexosBASE/src/main.tsx
@@ -1,14 +1,14 @@
-// src/main.tsx
+// main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import App from './App.tsx'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/annexos">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
