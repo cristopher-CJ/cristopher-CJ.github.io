2aded944cd03ec9c9f9b575acecc28dceb0404df
cristopher-CJ <christopher.barahona@aetech.biz>
2025-05-28 17:16:56 +0200

    SAMPLE PRUEBAS 30

---
+++ annexosBase/src/componentes/BottomBar.tsx
@@ -0,0 +1,13 @@
+// src/components/BottomBar.tsx
+import React from 'react'
+import './BottomBar.css'
+
+const BottomBar: React.FC = () => {
+  return (
+    <footer className="bottom-bar">
+      {/* Aquí puedes añadir contenido más adelante */}
+    </footer>
+  )
+}
+
+export default BottomBar
--- annexosBase/src/pages/SamplePage.tsx
+++ annexosBase/src/pages/SamplePage.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import cards from '../data/cards.json' // Importa directamente el JSON
 import './SamplePage.css'
+import BottomBar from '../componentes/BottomBar'
 
 const SamplePage: React.FC = () => {
   return (
-    <div className="sample-page-container">
-      {cards.map(({ id, title, content, date }) => (
-        <div className="card" key={id}>
-          <h3 className="card-title">{title}</h3>
-          <p className="card-content">{content}</p>
-          <div className="card-date">{date}</div>
+    <div>
+        <div className="sample-page-container">
+            {cards.map(({ id, title, content, date }) => (
+                <div className="card" key={id}>
+                <h3 className="card-title">{title}</h3>
+                <p className="card-content">{content}</p>
+                <div className="card-date">{date}</div>
+                </div>
+            ))}
         </div>
-      ))}
+
+        <BottomBar />
     </div>
   )
 }
