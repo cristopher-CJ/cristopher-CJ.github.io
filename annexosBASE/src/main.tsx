35c23603f5400840b773cbf49c3c00915513e452
cristopher-CJ <christopher.barahona@aetech.biz>
2025-05-28 11:30:49 +0200

    SAMPLE PRUEBAS 21

--- annexosBASE/src/main.tsx
+++ annexosBASE/src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/pruebas">
+    <BrowserRouter basename="/annexos">
       <App />
     </BrowserRouter>
   </React.StrictMode>
