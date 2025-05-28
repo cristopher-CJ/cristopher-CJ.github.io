189c489b6bad2f6dd2272d70fdd9a7e49813b098
cristopher-CJ <christopher.barahona@aetech.biz>
2025-05-28 17:08:03 +0200

    SAMPLE PRUEBAS 29

--- annexosBase/src/pages/SamplePage.tsx
+++ annexosBase/src/pages/SamplePage.tsx
@@ -1,8 +1,17 @@
+import React from 'react'
+import cards from '../data/cards.json' // Importa directamente el JSON
+import './SamplePage.css'
+
 const SamplePage: React.FC = () => {
   return (
-    <div>
-      <h1>Sample Page</h1>
-      <p>Esta es una página de ejemplo.</p>
+    <div className="sample-page-container">
+      {cards.map(({ id, title, content, date }) => (
+        <div className="card" key={id}>
+          <h3 className="card-title">{title}</h3>
+          <p className="card-content">{content}</p>
+          <div className="card-date">{date}</div>
+        </div>
+      ))}
     </div>
   )
 }
