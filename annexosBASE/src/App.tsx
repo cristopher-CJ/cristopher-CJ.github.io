eec7d38c69bec15a906a4ec1d35ae9126bc7e8fb
cristopher-CJ <christopher.barahona@aetech.biz>
2025-05-28 11:47:00 +0200

    SAMPLE PRUEBAS 22

--- annexosBASE/src/App.tsx
+++ annexosBASE/src/App.tsx
@@ -2,19 +2,27 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Inicio from './pages/Inicio'
 import SamplePage from './pages/SamplePage'
+import './App.css'
 
 const App: React.FC = () => {
   return (
     <div>
-      <nav style={{ padding: '10px', background: '#eee' }}>
-        <Link to="/" style={{ marginRight: '10px' }}>Inicio</Link>
-        <Link to="/sample">Sample Page</Link>
-      </nav>
+      <header className="navbar">
+        <div className="navbar-content">
+          <div className="navbar-title">Mi Mini Web</div>
+          <nav className="navbar-links">
+            <Link to="/">Inicio</Link>
+            <Link to="/sample">Sample Page</Link>
+          </nav>
+        </div>
+      </header>
 
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sample" element={<SamplePage />} />
-      </Routes>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sample" element={<SamplePage />} />
+        </Routes>
+      </main>
     </div>
   )
 }
