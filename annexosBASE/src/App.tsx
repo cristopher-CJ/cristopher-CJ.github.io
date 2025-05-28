c2351f301715a5dd6b3f0af33e916c76095a28cb
cristopher-CJ <christopher.barahona@aetech.biz>
2025-05-28 10:57:57 +0200

    SAMPLE PRUEBAS 17

--- annexosBASE/src/App.tsx
+++ annexosBASE/src/App.tsx
@@ -1,21 +1,34 @@
-// src/App.tsx
-import { Routes, Route, Link } from 'react-router-dom'
-import Inicio from './pages/Inicio'
-import SamplePage from './pages/SamplePage'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App: React.FC = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <nav style={{ padding: '10px', background: '#eee' }}>
-        <Link to="/" style={{ marginRight: '10px' }}>Inicio</Link>
-        <Link to="/sample">Sample Page</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sample" element={<SamplePage />} />
-      </Routes>
-    </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
